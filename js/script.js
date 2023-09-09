95f437fb1b9cb30a9d99e96b16a4c26cd178f5be
vishnugupta15 <0701cs211070@uecu.ac.in>
2023-09-09 23:09:44 +0530

    portfolio

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        // sticky navbar on scroll script
+        // sticky main_navbar on scroll script
         if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
+            $('.main_navbar').addClass("sticky");
         }else{
-            $('.navbar').removeClass("sticky");
+            $('.main_navbar').removeClass("sticky");
         }
         
         // scroll-up button show/hide script
@@ -22,14 +22,14 @@ $(document).ready(function(){
         $('html').css("scrollBehavior", "auto");
     });
 
-    $('.navbar .menu li a').click(function(){
+    $('.main_navbar .menu li a').click(function(){
         // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
 
-    // toggle menu/navbar script
+    // toggle menu/main_navbar script
     $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
+        $('.main_navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 
